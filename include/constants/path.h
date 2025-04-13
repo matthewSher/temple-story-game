c8693e3a96fee304de7821e37e98445a0b376c71
matthewSher <matvey.sher@yandex.ru>
2025-04-14 00:45:14 +0300

    Добавлен файл с константами путей

---
+++ include/constants/path.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+/**
+ * В файле определены основные локальные пути, используемые в проекте.
+ * Эти пути указывают на различные ресурсы (например, текстуры, карты тайлов и т.д.),
+ * которые используются в игре.
+ */
+
+const std::string TILESET_PATH = "assets\\tileset\\tileset.png"; // Путь к текстурам
+const std::string LEVEL_PATH = "assets\\levels\\";               // Путь к уровням
